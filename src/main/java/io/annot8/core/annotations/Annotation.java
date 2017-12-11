9b9a241f52370d96a0a2e5a0e808b0e0085042a7
Chris Flatley <chris@committed.io>
2017-12-11 11:14:37 +0000

    Add different content and bounds types

--- src/main/java/io/annot8/core/annotations/Annotation.java
+++ src/main/java/io/annot8/core/annotations/Annotation.java
@@ -1,6 +1,7 @@
 package io.annot8.core.annotations;
 
 
+import io.annot8.core.helpers.WithBounds;
 import io.annot8.core.helpers.WithId;
 import io.annot8.core.helpers.WithType;
 
@@ -11,6 +12,6 @@ import io.annot8.core.helpers.WithType;
  * history of an annotation (i.e. which processor created it, which
  * ones modified it, etc.)
  */
-public interface Annotation extends WithId, WithType {
+public interface Annotation extends WithId, WithType, WithBounds {
 
 }
