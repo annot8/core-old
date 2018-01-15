7d9b98b8ec4edb555f2a1229533080d832ed5bbb
James Baker <james.d.baker@gmail.com>
2018-01-15 20:49:05 +0000

    How does AnnotationCollection work?

--- src/main/java/io/annot8/core/annotations/AnnotationCollection.java
+++ src/main/java/io/annot8/core/annotations/AnnotationCollection.java
@@ -8,6 +8,7 @@ import io.annot8.core.helpers.WithId;
 import io.annot8.core.helpers.WithProperties;
 import io.annot8.core.helpers.WithType;
 
+//TODO: This needs more thought. How does it get stored? Is it linked to a specific view, or does it sit above them?
 public interface AnnotationCollection<B extends Bounds> extends WithId, WithType, WithProperties {
 	Stream<Annotation<B>> getAnnotations();
 	
@@ -20,4 +21,6 @@ public interface AnnotationCollection<B extends Bounds> extends WithId, WithType
 	default void removeAllAnnotations(Collection<Annotation<B>> annotations) {
 		annotations.forEach(this::removeAnnotation);
 	}
+	
+	boolean containsAnnotation(Annotation<B> annotation);
 }
