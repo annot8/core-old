b9d9ea40cd8a0201071754d88ea831e5f906440e
jamesdbaker <james.d.baker@gmail.com>
2018-01-29 09:59:41 +0000

    Add Builder to Content

--- src/main/java/io/annot8/core/data/Content.java
+++ src/main/java/io/annot8/core/data/Content.java
@@ -5,10 +5,21 @@ import io.annot8.core.stores.Annotations;
 
 public interface Content<D> extends WithTags {
 
-  // TODO: Do we want this...
   D getData();
 
   Annotations<?, ?> getAnnotations();
 
   String getName();
+
+  interface Builder<A extends Content<D>, D> {
+    Content.Builder<A, D> fromContent(Content content);
+
+    Content.Builder<A, D> setName(String name);
+    Content.Builder<A, D> setData(D content);
+
+    Content.Builder<A, D> setTag(String tag);
+    Content.Builder<A, D> setTags(Tags tags);
+
+    Content.Builder<A, D> setAnnotations(Annotations<?, ?> annotations);
+  }
 }
