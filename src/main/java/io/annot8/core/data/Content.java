cd75528ae73bf7f831057b1aca9c3d0af2137ab4
jamesdbaker <james.d.baker@gmail.com>
2018-01-29 10:47:36 +0000

    Add properties to Content

--- src/main/java/io/annot8/core/data/Content.java
+++ src/main/java/io/annot8/core/data/Content.java
@@ -1,10 +1,11 @@
 package io.annot8.core.data;
 
 import io.annot8.core.exceptions.IncompleteException;
+import io.annot8.core.helpers.WithProperties;
 import io.annot8.core.helpers.WithTags;
 import io.annot8.core.stores.Annotations;
 
-public interface Content<D> extends WithTags {
+public interface Content<D> extends WithTags, WithProperties {
 
   D getData();
 
@@ -20,6 +21,9 @@ public interface Content<D> extends WithTags {
 
     Content.Builder<A, D> setTag(String tag);
     Content.Builder<A, D> setTags(Tags tags);
+
+    Content.Builder<A, D> setProperty(String key, Object value);
+    Content.Builder<A, D> setProperties(Properties properties);
 
     Content.Builder<A, D> setAnnotations(Annotations<?, ?> annotations);
 
