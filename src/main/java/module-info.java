f26aa6dde48590e457cb2572fcbc9362def319f9
jamesdbaker <james.d.baker@gmail.com>
2018-02-02 13:45:23 +0000

    Java 9 module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module io.annot8.core {
+    exports io.annot8.core.annotations;
+    exports io.annot8.core.bounds;
+    exports io.annot8.core.components;
+    exports io.annot8.core.context;
+    exports io.annot8.core.data;
+    exports io.annot8.core.exceptions;
+    //io.annot8.core.helpers is not exported
+    exports io.annot8.core.properties;
+    exports io.annot8.core.settings;
+    exports io.annot8.core.stores;
+}
